2d1b9877403fbe8b5494ebbd6ee80ae1e1f3ebf3
Bartosz Skiba <bartek2912@hotmail.com>
2018-05-28 17:24:36 +0200

    Created class Game for sf::Texture and added Cons locations to files

---
+++ Projekt/Potwory/Potwory/Constances.h
@@ -0,0 +1,5 @@
+#pragma once
+#include"stdafx.h"
+
+enum files{HEART,BLACKHEART,SWORD,CHARACTER,MONSTER,WALL};
+const std::string filesLocations[] = {"..//sprites//heart.png","..//sprites//blackheart.png","..//sprites//sword.png", "..//sprites//character.png","..//sprites//monster.png", "..//sprites//wall.png"};
--- Projekt/Potwory/Potwory/Game.h
+++ Projekt/Potwory/Potwory/Game.h
@@ -7,3 +7,21 @@
 //Small_Monster
 //Large_Monster
 #pragma once
+#include"stdafx.h"
+#include"Constances.h"
+class Game
+{
+public:
+//constructors and destructors
+	Game();
+	~Game();
+//getters
+
+//setters
+	//void SetTexture(const std::string &fileName);//Loading Texture from file
+private:
+	std::vector<sf::Texture> textures;
+};
+
+
+
