25fcfb6d7e29f95f3fb0103a961f3288a349c623
Bartosz Skiba <bartek2912@hotmail.com>
2018-05-29 20:21:12 +0200

    Added default window size

--- Projekt/Potwory/Potwory/Constances.h
+++ Projekt/Potwory/Potwory/Constances.h
@@ -3,3 +3,4 @@
 
 enum files{HEART,BLACKHEART,SWORD,CHARACTER,MONSTER,WALL};
 const std::vector<std::string> fileLocations = {"..//sprites//heart.png","..//sprites//blackheart.png","..//sprites//sword.png", "..//sprites//character.png","..//sprites//monster.png", "..//sprites//wall.png"};
+const sf::VideoMode DEFAULT_SIZE_VIDEO = { 1366,768 };
