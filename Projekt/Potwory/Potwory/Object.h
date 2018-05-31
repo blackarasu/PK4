8f82cb2febae88be88a8d1571c512b976919bc1e
Magda <Magda@MARTYNA-PC>
2018-05-31 17:24:58 +0200

    changed method DrawToWindow to virtual method

--- Projekt/Potwory/Potwory/Object.h
+++ Projekt/Potwory/Potwory/Object.h
@@ -26,7 +26,7 @@ public:
 	std::string GetID();
 //Methods
 	//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	void DrawToWindow(sf::RenderWindow *window); //!!!!!!Find other way to DrawToWindow because it is not scallable!!!!!!!!!!!!!!!!!!!!!!!!! //Optimal only for the same size of sprites
+	virtual void DrawToWindow(sf::RenderWindow *window); //!!!!!!Find other way to DrawToWindow because it is not scallable!!!!!!!!!!!!!!!!!!!!!!!!! //Optimal only for the same size of sprites
 	//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	virtual void DoAction() = 0; //future object's Action/-s
 	virtual void DoAction(int &valueToChange)=0; //object's Action/-s operated with referenced value to change
