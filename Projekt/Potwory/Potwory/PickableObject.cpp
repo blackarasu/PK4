5aa9226f4a0a4296199b652bc64ebc4a5b6655ac
Magda <Magda@MARTYNA-PC>
2018-05-31 19:13:04 +0200

    Added enum Picked, vector of sf::Sprite, methods PickedMe , const values NOT_PICKED_RECT and PICKED_RECT and overrided SetSprites for 2 Sprites also changed DrawPickableObject method

--- Projekt/Potwory/Potwory/PickableObject.cpp
+++ Projekt/Potwory/Potwory/PickableObject.cpp
@@ -15,7 +15,25 @@ Pickable::~Pickable()
 	}
 }
 
-void Pickable::DrawPickableObject(sf::RenderWindow * window)
+void Pickable::SetPlayerPosition(sf::Vector2f * playerPosition)
+{
+	this->playerPosition = playerPosition;
+}
+
+void Pickable::SetSprites(const sf::Texture & texture)
+{
+	this->sprites.push_back(sf::Sprite(texture, NOT_PICKED_RECT));
+	this->sprites.push_back(sf::Sprite(texture, PICKED_RECT));
+	this->sprites[PICKED].scale(0.25f, 0.25f);
+	this->SetActualSprite(&(this->sprites[NOT_PICKED]));
+}
+
+sf::Vector2f * Pickable::GetPlayerPosition()
+{
+	return this->playerPosition;
+}
+
+void Pickable::DrawPickableObject(sf::RenderWindow * window, sf::IntRect &sizeOfPicker)
 {
 	if (this->isPicked == false)
 	{
@@ -23,16 +41,16 @@ void Pickable::DrawPickableObject(sf::RenderWindow * window)
 	}
 	else
 	{
-		DrawToWindow(window, &(this->playerPosition()));
+		sf::Vector2f fixedPosition = *(this->playerPosition);
+		fixedPosition.x += sizeOfPicker.width * 0.5f;
+		fixedPosition.y += sizeOfPicker.height * 0.5f;
+		DrawToWindow(window, this->playerPosition);
 	}
 }
 
-void Pickable::SetPlayerPosition(sf::Vector2f * playerPosition)
+void Pickable::PickedMe(sf::Vector2f *playerPositon)
 {
+	this->isPicked = true;
+	this->SetActualSprite(&(this->sprites[PICKED]));
 	this->playerPosition = playerPosition;
-}
-
-sf::Vector2f * Pickable::GetPlayerPosition()
-{
-	return this->playerPosition;
 }
--- Projekt/Potwory/Potwory/PickableObject.h
+++ Projekt/Potwory/Potwory/PickableObject.h
@@ -3,20 +3,27 @@
 #include "stdafx.h"
 #include "Object.h"
 
+const sf::IntRect NOT_PICKED_RECT = { 0,0,16,16 };
+const sf::IntRect PICKED_RECT = { 17,0,16,16 };
 class Pickable :public Object
 {
 public:
 //constructors and destructors
 	Pickable();
 	~Pickable();
-//methods
-	void DrawPickableObject(sf::RenderWindow *window);
+
 //setters
-	void SetPlayerPosition(sf::Vector2f *playerPosition);
+	void SetPlayerPosition(sf::Vector2f *playerPosition); //set address to playerPosition
+	void SetSprites(const sf::Texture &texture) override; //overrided method for SetSprites. It sets two sprites for NOT_PICKED and PICKED version;
 //getters 
 	sf::Vector2f *GetPlayerPosition();
+//methods
+	void DrawPickableObject(sf::RenderWindow *window, sf::IntRect &sizeOfPicker); //different function for drawing PICKED and NOT_PICKED object
+	void PickedMe(sf::Vector2f *playerPositon); //Someone picked Pickable Object
 private:
+	enum Picked{NOT_PICKED, PICKED};
 	bool isPicked;
 	sf::Vector2f *playerPosition;
+	std::vector<sf::Sprite> sprites;
 };
 
