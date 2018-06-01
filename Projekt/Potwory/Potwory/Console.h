fc62f81372eb3efd30296c107aebbbbf722fa976
Magda <Magda@MARTYNA-PC>
2018-06-01 20:49:41 +0200

    Thinking about deleting this file/class

--- Projekt/Potwory/Potwory/Console.h
+++ Projekt/Potwory/Potwory/Console.h
@@ -1,3 +1,4 @@
 //Zarz¹dzanie ca³ym ekranem, bufforem, wyœwietlanie konkretnych wartsw
 //Konkretnej klatki/ramki
+//About to delete from project because of sf::RenderWindow class wich does the same job.
 #pragma once
