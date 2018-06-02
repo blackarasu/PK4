40bac51ab733f2776505b83bae32fea53eb014ca
Magda <Magda@MARTYNA-PC>
2018-06-03 01:16:17 +0200

    overrided method bool DoAction() and added field range + some getters and setters

--- Projekt/Potwory/Potwory/Sword.h
+++ Projekt/Potwory/Potwory/Sword.h
@@ -7,13 +7,24 @@ public:
 //constructors and destructors
 	Sword();
 	Sword(const float &x, const float &y, const sf::Texture &texture);
-	Sword(const float &x, const float &y, const sf::Texture &texture, const float &damage, const float &endurance, const float &attackSpeed);
-	Sword(const sf::Texture &texture, sf::Vector2f *pickedPosition, const float &damage, const float &endurance, const float &attackSpeed);
+	Sword(const float &x, const float &y, const sf::Texture &texture, const float &damage, const float &endurance, const float &attackSpeed, const float &range);
+	Sword(const sf::Texture &texture, sf::Vector2f *pickedPosition, const float &damage, const float &endurance, const float &attackSpeed, const float &range);
 	~Sword();
 //setters
+	void SetDamage(const float &damage);
+	void SetEnduramce(const float &endurance);
+	void SetAttackSpeed(const float &attackSpeed);
+	void SetRange(const float &range);
+	void SetAttributes(const float &damage, const float &endurance, const float &attackSpeed, const float &range);
 //getters
+	float GetDamage();//dmg dealt every attack
+	float GetAttackSpeed();//how many times per second you can use attack
+	float GetRange();//Range in pixels
+//methods
+	bool DoAction() override;
 private:
 	float damage; //dmg dealt in every attack
 	float endurance;//how much attacks to destroy (even not hit)
 	float attackSpeed; //how fast you can attack with that Sword (How many times per second you can use attack)
+	float range; //range in pixels;
 };
