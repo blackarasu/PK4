7f2af32876c09c7855a08bd36fefc751fe522d66
Magda <Magda@MARTYNA-PC>
2018-06-03 17:23:14 +0200

    Created whole class Dynamic, enum Frame consts DIRECTIONS, ANIMATION_FRAMES, SPRITES_POSITIONS,

--- Projekt/Potwory/Potwory/Dynamic_Object.cpp
+++ Projekt/Potwory/Potwory/Dynamic_Object.cpp
@@ -4,9 +4,22 @@
 Dynamic::Dynamic():Object(0,0,"I'm nothing")
 {
 	this->hp = 100;
-	this->speed.x = 150;
-	this->speed.y = 150;
+	this->speed.x = 150.0f;
+	this->speed.y = 150.0f;
 	this->item = nullptr;
+	this->lastMove = Direction::DOWN;
+	this->frame = Frame::STOP;
+}
+
+Dynamic::Dynamic(const float & x, const float & y, const std::string & ID, const sf::Texture & texture):Object(x,y,ID)
+{
+	SetSprites(texture);
+	FixPosition();
+	this->hp = 100;
+	this->speed.x = 150.0f;
+	this->speed.y = 150.0f;
+	this->item = nullptr;
+	this->lastMove = Direction::DOWN;
 }
 
 Dynamic::~Dynamic()
@@ -15,4 +28,107 @@ Dynamic::~Dynamic()
 	{
 		delete this->item;
 	}
+}
+
+void Dynamic::SetSprites(const sf::Texture & texture)
+{
+	for (auto i = 0; i < DIRECTIONS; ++i)
+	{
+		for (auto j = 0; j < ANIMATION_FRAMES; ++j)
+		{
+			this->sprites[i][j] = sf::Sprite(texture, SPRITES_POSITION[i][j]);
+		}
+	}
+	SetActualSprite(&(this->sprites[Direction::DOWN][1]));
+}
+
+void Dynamic::SetHP(const int & hp)
+{
+	this->hp = hp;
+}
+
+int Dynamic::GetHP()
+{
+	return this->hp;
+}
+
+unsigned int Dynamic::GetLastMove()
+{
+	return this->lastMove;
+}
+
+void Dynamic::MoveUp(float frameTime)
+{
+	this->pixelsPositon.y -= frameTime * speed.y;
+	if (this->lastMove == Direction::UP)
+	{
+		this->frame++;
+		if (this->frame > Frame::THIRD)
+		{
+			this->frame = Frame::FIRST;
+		}
+	}
+	else
+	{
+		this->lastMove = Direction::UP;
+		this->frame = Frame::FIRST;
+	}
+	SetActualSprite(&(this->sprites[this->lastMove][this->frame]));
+}
+
+void Dynamic::MoveDown(float frameTime)
+{
+	this->pixelsPositon.y += frameTime * speed.y;
+	if (this->lastMove == Direction::DOWN)
+	{
+		this->frame++;
+		if (this->frame > Frame::THIRD)
+		{
+			this->frame = Frame::FIRST;
+		}
+	}
+	else
+	{
+		this->lastMove = Direction::DOWN;
+		this->frame = Frame::FIRST;
+	}
+	SetActualSprite(&(this->sprites[this->lastMove][this->frame]));
+}
+
+void Dynamic::MoveLeft(float frameTime)
+{
+	this->pixelsPositon.x -= frameTime * speed.x;
+	if (this->lastMove == Direction::LEFT)
+	{
+		this->frame++;
+		if (this->frame > Frame::THIRD)
+		{
+			this->frame = Frame::FIRST;
+		}
+	}
+	else
+	{
+		this->lastMove = Direction::LEFT;
+		this->frame = Frame::FIRST;
+	}
+	SetActualSprite(&(this->sprites[this->lastMove][this->frame]));
+}
+
+void Dynamic::MoveRight(float frameTime)
+{
+	this->pixelsPositon.x += frameTime * speed.x;
+	if (this->lastMove == Direction::RIGHT)
+	{
+		this->frame++;
+		if (this->frame > Frame::THIRD)
+		{
+			this->frame = Frame::FIRST;
+		}
+	}
+	else
+	{
+		this->lastMove = Direction::RIGHT;
+		this->frame = Frame::FIRST;
+	}
+	SetActualSprite(&(this->sprites[this->lastMove][this->frame]));
 }
--- Projekt/Potwory/Potwory/Dynamic_Object.h
+++ Projekt/Potwory/Potwory/Dynamic_Object.h
@@ -3,3 +3,50 @@
 //Ró¿ne sprite'y
 //Iloœæ zdrowia
 #pragma once
+#include "stdafx.h"
+#include "Object.h"
+
+enum Frame :unsigned int { FIRST, STOP, THIRD };
+const unsigned int DIRECTIONS = 4;
+const unsigned int ANIMATION_FRAMES = 3;
+const sf::IntRect SPRITES_POSITION[DIRECTIONS][ANIMATION_FRAMES] = {
+	sf::IntRect({ 0,0,16,16 }),sf::IntRect({ 0,0,16,16 }),sf::IntRect({ 0,0,16,16 }),
+	sf::IntRect({ 0,0,16,16 }),sf::IntRect({ 0,0,16,16 }),sf::IntRect({ 0,0,16,16 }),
+	sf::IntRect({ 0,0,16,16 }),sf::IntRect({ 0,0,16,16 }),sf::IntRect({ 0,0,16,16 }),
+	sf::IntRect({ 0,0,16,16 }),sf::IntRect({ 0,0,16,16 }),sf::IntRect({ 0,0,16,16 })
+};//fill when sprites will be ready
+
+class Dynamic:public Object
+{
+public:
+//constructors destructors
+	Dynamic();
+	Dynamic(const float &x, const float &y, const std::string &ID, const sf::Texture &texture);
+	~Dynamic();
+//setters
+	void SetSprites(const sf::Texture &texture)override;
+	void SetHP(const int &hp);
+	template <class T>
+	void Pick(T* item);//Use only derivatives of the Object
+//getters
+	int GetHP();
+	unsigned int GetLastMove();
+//methods
+	void MoveUp(float frameTime);
+	void MoveDown(float frameTime);
+	void MoveLeft(float frameTime);
+	void MoveRight(float frameTime);
+private:
+	int hp; 
+	sf::Vector2f speed; //how many pixels u can go through one second
+	Object *item; //address to picked item if you picked one
+	sf::Sprite sprites[DIRECTIONS][ANIMATION_FRAMES];
+	unsigned int lastMove;
+	int frame;
+};
+
+template<class T>
+inline void Dynamic::Pick(T * item)
+{
+	this->item = item;
+}
