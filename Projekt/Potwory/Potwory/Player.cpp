0231613d632c16e9fb456ad7e8d66e33d2830702
Bartosz Skiba <bartek2912@hotmail.com>
2018-06-05 19:19:19 +0200

    Added Move method

--- Projekt/Potwory/Potwory/Player.cpp
+++ Projekt/Potwory/Potwory/Player.cpp
@@ -5,8 +5,9 @@ Player::Player():Dynamic()
 {
 }
 
-Player::Player(const float & x, const float & y, const sf::Texture & texture, const int & hp, const sf::Vector2f & speed) : Dynamic(x, y, "Player", texture, hp, speed)
+Player::Player(const float & x, const float & y, const sf::Texture & texture, const int & hp, const sf::Vector2f & speed/*, sf::Event * event*/) : Dynamic(x, y, "Player", texture, hp, speed)
 {
+	//this->event = event;
 }
 
 Player::~Player()
@@ -21,3 +22,28 @@ bool Player::DoAction()
 	}
 	return false;
 }
+
+void Player::Move(float frameTime)
+{
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+	{
+		MoveLeft(frameTime);
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+	{
+		MoveDown(frameTime);
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+	{
+		MoveUp(frameTime);
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+	{
+		MoveRight(frameTime);
+	}
+	else
+	{
+		NoMove();
+	}
+}
+		
--- Projekt/Potwory/Potwory/Player.h
+++ Projekt/Potwory/Potwory/Player.h
@@ -11,12 +11,13 @@ class Player:public Dynamic
 public:
 //Constructors & Destructors
 	Player();
-	Player(const float &x, const float &y, const sf::Texture &texture, const int &hp, const sf::Vector2f &speed);
+	Player(const float &x, const float &y, const sf::Texture &texture, const int &hp, const sf::Vector2f &speed/*, sf::Event *event*/);
 	~Player();
 //Methods
 	bool DoAction()override;//action for pickable (wearable)
 	void DoAction(sf::Vector2f &valueToChange, const int &lastAction)override {}
 	void DoAction(int &hp) override {}
+	void Move(float frameTime);
 private:
-
+	//sf::Event *event;
 };
