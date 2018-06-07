6beaa92848a37de6acb98fdbf16ad7e11277eec3
Bartosz Skiba <bartek2912@hotmail.com>
2018-06-07 18:39:41 +0200

    overrided Attack method, created class Monster:public Dynamic

---
+++ Projekt/Potwory/Potwory/Monster.cpp
@@ -0,0 +1,80 @@
+#include "stdafx.h"
+#include "Monster.h"
+
+Monster::~Monster()
+{
+}
+
+void Monster::SetDamage(const float & damage)
+{
+	this->damage = damage;
+}
+
+void Monster::SetRange(const float & range)
+{
+	this->range = range;
+}
+
+void Monster::SetAttackSpeed(const float & attackSpeed)
+{
+	this->attackSpeed = attackSpeed;
+}
+
+float Monster::GetDamage()
+{
+	return this->damage;
+}
+
+float * Monster::GetAddressDamage()
+{
+	return &(this->damage);
+}
+
+float Monster::GetRange()
+{
+	return this->range;
+}
+
+float * Monster::GetAddressRange()
+{
+	return &(this->range);
+}
+
+float Monster::GetAttackSpeed()
+{
+	return this->attackSpeed;
+}
+
+float * Monster::GetAddressAttackSpeed()
+{
+	return &(this->attackSpeed);
+}
+
+sf::FloatRect Monster::Attack()//atakowanie troche podobne do playerAttack
+{
+	sf::FloatRect attackRectangle;
+	if (this->clock.getElapsedTime().asSeconds() > (1 / this->attackSpeed))
+	{
+		attackRectangle = this->GetActualSpriteAddress()->getGlobalBounds();
+		switch (GetLastMove())
+		{
+		case Direction::LEFT:
+			attackRectangle.left -= attackRectangle.width;
+			break;
+		case Direction::DOWN:
+			attackRectangle.top += attackRectangle.height;
+			break;
+		case Direction::UP:
+			attackRectangle.top -= attackRectangle.height;
+			break;
+		case Direction::RIGHT:
+			attackRectangle.left += attackRectangle.width;
+			break;
+		}
+		attackRectangle.height = this->range;
+		attackRectangle.width = this->range;
+		this->clock.restart();
+		return attackRectangle;
+	}
+	return attackRectangle; //else
+}
--- Projekt/Potwory/Potwory/Monster.h
+++ Projekt/Potwory/Potwory/Monster.h
@@ -1,3 +1,34 @@
 //AI potwora, wyznaczona trasa potwora po ktorej sie porusza domyœlnie
 //Pole wizji po którym bêdzie atakowa³
 #pragma once
+#include"stdafx.h"
+#include"Dynamic_Object.h"
+
+class Monster:public Dynamic
+{
+public:
+//Constructors destructors
+	Monster();
+	~Monster();
+//setters
+	void SetDamage(const float &damage);
+	void SetRange(const float &range);
+	void SetAttackSpeed(const float &attackSpeed);
+//getters
+	float GetDamage();
+	float *GetAddressDamage();//good for referance argument
+	float GetRange();
+	float *GetAddressRange();//good for referance argument
+	float GetAttackSpeed();
+	float *GetAddressAttackSpeed();//good for referance argument
+//methods
+	virtual sf::FloatRect Attack() override;
+//virtual
+	virtual void Decide(); //Monster AI
+private:
+//Fields
+	sf::Clock clock;
+	float damage;//monster's damage
+	float range; //monster's range
+	float attackSpeed;//how many attacks per second
+};
