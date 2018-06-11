723019383af699f44a74a1bf1fc903fc0921b3e5
Bartosz Skiba <bartek2912@hotmail.com>
2018-06-11 23:41:22 +0200

    fixed fix position (now takes proper width and height for sprite)

--- Projekt/Potwory/Potwory/Object.cpp
+++ Projekt/Potwory/Potwory/Object.cpp
@@ -145,7 +145,7 @@ void Object::DrawToWindow(sf::RenderWindow * window, sf::Vector2f *position)
 
 inline void Object::FixPosition()
 {
-	sf::IntRect sizeOfSprite = this->actualSprite->getTextureRect();
+	sf::FloatRect sizeOfSprite = this->actualSprite->getGlobalBounds();
 	SetPosition(float(sizeOfSprite.width)*this->pixelsPositon.x, float(sizeOfSprite.height)*this->pixelsPositon.y);
 	if (actualSprite != nullptr)
 	{
